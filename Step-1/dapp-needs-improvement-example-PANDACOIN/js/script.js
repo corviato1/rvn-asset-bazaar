564aa5872809b52a08c4fc525a157185f772d6a7
corviato1 <44780076+corviato1@users.noreply.github.com>
2021-04-01 20:51:32 -0700

    Add files via upload

---
+++ Step-1/dapp-needs-improvement-example-PANDACOIN/js/script.js
@@ -0,0 +1,19 @@
+// First Column Content
+document.getElementById("col-1-text-1").innerHTML = "PANDACOIN";
+document.getElementById("col-1-text-2").innerHTML = "QmNWwhYkTsqBJgGwPuhapnzB3mt2oEMRwfbipQJVXfhL9m";
+document.getElementById("col-1-text-2").href = "https://ipfs.io/ipfs/QmNWwhYkTsqBJgGwPuhapnzB3mt2oEMRwfbipQJVXfhL9m";
+document.getElementById("image-1").src = "assets/QmXcUxcw47obV9S7VBhN8sWxGrmM5eTjWbJLXtskRXDLvN.png";
+// document.getElementById("img-1").style.backgroundImage =
+//   "url('assets/image1.jpg')";
+// Second Column Content
+document.getElementById("col-2-text-1").innerHTML = "10 PANDACOIN for 1 RVN";
+document.getElementById("col-2-text-2").innerHTML = "RShhrqUEaDWJYkVeAGMJkzRHjykpxpoMc1";
+document.getElementById("col-2-text-3").innerHTML = "Asset Explorer";
+document.getElementById("col-2-text-3").href = "https://ravencoin.asset-explorer.net/a/PANDACOIN";
+document.getElementById("image-2").src = "assets/RShhrqUEaDWJYkVeAGMJkzRHjykpxpoMc1.png";
+// document.getElementById("img-2").style.backgroundImage =
+//   "url('assets/image2.jpeg')";
+
+
+
+//https://ipfs.io/ipfs/QmNWwhYkTsqBJgGwPuhapnzB3mt2oEMRwfbipQJVXfhL9m

344580ecea2ef7e9648c6a81932f91809cef3c32
corviato1 <44780076+corviato1@users.noreply.github.com>
2021-04-01 20:48:08 -0700

    Add files via upload

---
+++ Step-1/dapp-needs-improvement/js/script.js
@@ -0,0 +1,19 @@
+// First Column Content
+document.getElementById("col-1-text-1").innerHTML = "<Asset name>";
+document.getElementById("col-1-text-2").innerHTML = "<IPFS hash of asset>";
+document.getElementById("col-1-text-2").href = "https://ipfs.io/ipfs/<hash of asset>";
+document.getElementById("image-1").src = "assets/<file name of asset image>";
+// document.getElementById("img-1").style.backgroundImage =
+//   "url('assets/image1.jpg')";
+// Second Column Content
+document.getElementById("col-2-text-1").innerHTML = "<qty of asset> <asset name> for <qty of RVN price> RVN";
+document.getElementById("col-2-text-2").innerHTML = "<address to send RVN>";
+document.getElementById("col-2-text-3").innerHTML = "Asset Explorer";
+document.getElementById("col-2-text-3").href = "<Asset Explorer url>";
+document.getElementById("image-2").src = "assets/<file name of qr code image>";
+// document.getElementById("img-2").style.backgroundImage =
+//   "url('assets/image2.jpeg')";
+
+
+
+//https://ipfs.io/ipfs/QmNWwhYkTsqBJgGwPuhapnzB3mt2oEMRwfbipQJVXfhL9m

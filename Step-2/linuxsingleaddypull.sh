2ffbf5b135dc80a39f4c54eaf1f3bdac66fb988e
Alexander Dan <40121410+apdan510@users.noreply.github.com>
2021-04-02 14:17:23 -0700

    Create linuxsingleaddypull.sh

---
+++ Step-2/linuxsingleaddypull.sh
@@ -0,0 +1,5 @@
+
+
+#SINGLE PULL FOR TXIDS
+addys=$(curl -sS "https://ravencoin.network/api/tx/d67e507277e1a3f8b3f399ab4d0b479e90f63c06a470c50b0fe0020bdd504c51")
+echo "$addys" | jq '.vout' > singlerawaddy.txt
